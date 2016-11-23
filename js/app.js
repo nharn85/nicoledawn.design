37543e1aed350d338127d26053556fb4f62deb84
nmacd85 <nmacd85@gmail.com>
2016-11-23 06:06:47 -0400

    v2.0

--- js/app.js
+++ js/app.js
@@ -3,6 +3,6 @@ var App = angular.module('app', []);
 App.controller('GridCtrl', function($scope, $http) {
   $http.get('data.json')
        .then(function(res){
-          $scope.gridItems = res.data;
+          $scope.gridItems = res.data.designs;
         });
 });
